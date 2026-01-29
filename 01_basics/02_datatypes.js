fa1c829b3225b613cc23b11921e12d146567cc07
PrajwalThakur12345 <prajwallittlu@gmail.com>
2026-01-29 15:43:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  // treat all JS code as newer version of javascript
+
+// alert(3 + 3) // we are using node.js not browser so it will not run.
+
+
+
+
+// datatypes.
+let name = "hitesh" //  string.
+let age = 18 // number
+let isloggedIn = false // boolean
+
+// number max upto 2^53
+// null => standlone value; // represent empty value.
+// undefined => no value is assigned so it is undefined 
+//symbol => for unique we need symbol datatype.
+
+
+// 
+
+console.log(typeof "Prajwal"); // output is string
+console.log(typeof null); // output is object
+comsole.log(typeof undefined); // output is undefined
